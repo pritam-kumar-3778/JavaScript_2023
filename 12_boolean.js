be3a6423f89dfbe10b7f42a85b283128a3e3285a
Pritam Kumar Patel <127990174+pritam-kumar-3778@users.noreply.github.com>
2023-08-25 01:57:27 +0530

    Delete 12_boolean.js

--- 12_boolean.js
+++
@@ -1,9 +0,0 @@
-let age1 = 23;
-let isAdult1 = true;
-// console.log(isAdult1);
-console.log(typeof isAdult1); // boolean
-
-let age2 = 13;
-let isAdult2 = false;
-// console.log(isAdult2);
-console.log(typeof isAdult2); //boolean
