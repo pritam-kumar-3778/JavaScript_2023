87257f74a2a8b49fc602c4f50c13ba9f150016e6
Pritam Kumar Patel <127990174+pritam-kumar-3778@users.noreply.github.com>
2023-08-25 01:56:36 +0530

    Delete 11_identifiers rule.js

--- 11_identifiers rule.js
+++
@@ -1,8 +0,0 @@
-// Varieables names contain letters,digit,underscore and dollar can't start with digit
-let first_Name = "Pritam";
-let _middle_Name = "Kumar";
-let $last_name = "Patel";
-
-console.log(first_Name);
-console.log(_middle_Name);
-console.log($last_name);
