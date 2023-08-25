65deccc1b97f3635aad77eecd8ee1d10ede1aa5f
Pritam Kumar Patel <127990174+pritam-kumar-3778@users.noreply.github.com>
2023-08-26 01:45:23 +0530

    Add files via upload

---
+++ JS_part_2/25_truthy_falsy.js
@@ -0,0 +1,71 @@
+if(true){
+    console.log("It ha true value");
+} else {
+    console.log("It has false value");
+}
+
+if(1){
+    console.log("It ha true value");
+} else {
+    console.log("It has false value");
+}
+
+if(false){
+    console.log("It ha true value");
+} else {
+    console.log("It has false value");
+}
+
+if(0){
+    console.log("It ha true value");
+} else {
+    console.log("It has false value");
+}
+
+if(-2){
+    console.log("It ha true value");
+} else {
+    console.log("It has false value");
+}
+
+if(-0){
+    console.log("It ha true value");
+} else {
+    console.log("It has false value");
+}
+
+if(0n){ //bigint value
+    console.log("It ha true value");
+} else {
+    console.log("It has false value");
+}
+
+if(""){
+    console.log("It ha true value");
+} else {
+    console.log("It has false value");
+}
+
+if("Really I enjoy to learn JavaScript."){
+    console.log("It ha true value");
+} else {
+    console.log("It has false value");
+}
+
+if(null){
+    console.log("It ha true value");
+} else {
+    console.log("It has false value");
+}
+
+if(undefined){
+    console.log("It ha true value");
+} else {
+    console.log("It has false value");
+}
+
+if(NaN){
+    console.log("It ha true value");
+} else {
+    console.log("It has false value");
+}
